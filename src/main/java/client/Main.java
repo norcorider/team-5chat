e4fe850c17a007795476d3a5340569f87c3f3b49
klu <klu44@list.ru>
2017-10-07 18:35:28 +1000

    Small reworking. Add checkd message lenght.

--- src/main/java/client/Main.java
+++ src/main/java/client/Main.java
@@ -7,7 +7,7 @@ import java.util.Date;
 import java.util.Scanner;
 
 public class Main {
-    private static final String IP = "192.186.0.62";
+    private static final String IP = "localhost";
     private static final int PORT = 60000;
 
     public static void main(String[] args) {
@@ -27,8 +27,14 @@ public class Main {
             }).start();
             while (true) {
                 String message = in.nextLine();
-                if((message.length() >= 5) && (message.substring(0,5).equals("/snd ")))
+                if(message.length() > 155)
+                {
+                    System.out.println("Too large lenght");
+                } else if((message.length() >= 5) && (message.substring(0,5).equals("/snd ")))
                     out.println((new Date()) + ": " + message.substring(5));
+                else {
+                    System.out.println("Incorrect command");
+                }
             }
 
         } catch (IOException e) {
