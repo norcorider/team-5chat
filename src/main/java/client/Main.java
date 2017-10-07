daf9fd666181a6654eb4b44ba6ac3383f4f2a46f
klu <klu44@list.ru>
2017-10-07 18:26:04 +1000

    IP and PORT

--- src/main/java/client/Main.java
+++ src/main/java/client/Main.java
@@ -7,9 +7,12 @@ import java.util.Date;
 import java.util.Scanner;
 
 public class Main {
+    private static final String IP = "192.186.0.62";
+    private static final int PORT = 60000;
+
     public static void main(String[] args) {
         try {
-            Socket client = new Socket("localhost", 60000);
+            Socket client = new Socket(IP, PORT);
             PrintWriter out = new PrintWriter(client.getOutputStream(), true);
             BufferedReader reader = new BufferedReader(new InputStreamReader(client.getInputStream()));
             Scanner in = new Scanner(System.in);
