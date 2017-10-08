e5c3d5f06085100e87339ce5156b88e90e222b95
dy.samsonov <dy.samsonov@jet.msk.su>
2017-10-08 12:21:56 +0300

    message

--- src/main/java/su/jet/team05/chat/server/Message.java
+++ src/main/java/su/jet/team05/chat/server/Message.java
@@ -1,26 +1,49 @@
 package su.jet.team05.chat.server;
 
 
-import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
-public class Message {
+public class Message{ //implements Comparable<Message>{
 
-    private Date dateMessage;
+    private SimpleDateFormat dateMessage;
     private String userName;
     private String message;
+    private static int countAnon = 0;
     public Message(String userName, String message){
         this.userName = userName;
         this.message = message;
-        dateMessage = new Date();
+        dateMessage = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+    }
+
+    public Message(String message){
+        countAnon++;
+        this.userName = "anonymous" + countAnon;
+        this.message = message;
+        dateMessage = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
     }
 
 
 
     public String getMessage() {
-        String strDate = DateFormat.getDateInstance(DateFormat.SHORT).format(dateMessage);
-
-        return
+            String result;
+            Date now = new Date();
+            String strDate = dateMessage.format(now);
+            result = strDate + " " + userName + ": " + message;
+            return result;
     }
 
+   /* @Override
+    public int compareTo(Message o) {
+        int  b;
+        if (this.dateMessage > o.dateMessage)
+            b = 1;
+            if(this.dateMessage == o.dateMessage)
+            b = -1;
+        else b = 0;
+        return b;
+
+    }*/
+
 }
+
