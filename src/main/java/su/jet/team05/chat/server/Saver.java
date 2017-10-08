9083cb147686ebe16f1c09c9d89ab07d92b01452
Madina Deryagina <madina.deryagina@yandex.ru>
2017-10-08 14:30:22 +0300

    история в сэйвер

--- src/main/java/su/jet/team05/chat/server/Saver.java
+++ src/main/java/su/jet/team05/chat/server/Saver.java
@@ -1,12 +1,23 @@
 package su.jet.team05.chat.server;
 
+import java.io.PrintWriter;
+import java.util.LinkedList;
+import java.util.List;
+
 public class Saver {
     private static final String path = "";
+    private static List<Message> history = new LinkedList<>();
+
     static void saveMessage(Message message) {
         //Сохраняем сообщения, сохраняет в файлик объект
+        history.add(message);
+
     }
-    static String getHistory() {
+    static String getHistory(PrintWriter pw2) {
         //Читаем пообъектно.
+        for (Message oldMessage : history) {
+            pw2.println(oldMessage);
+        }
         return "";
     }
 
