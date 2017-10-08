c10031508c5820de9b3d46211cc8bc581951490b
dy.samsonov <dy.samsonov@jet.msk.su>
2017-10-08 11:03:10 +0300

    serv mess

--- src/main/java/su/jet/team05/chat/server/Message.java
+++ src/main/java/su/jet/team05/chat/server/Message.java
@@ -1,4 +1,26 @@
 package su.jet.team05.chat.server;
 
+
+import java.text.DateFormat;
+import java.util.Date;
+
 public class Message {
+
+    private Date dateMessage;
+    private String userName;
+    private String message;
+    public Message(String userName, String message){
+        this.userName = userName;
+        this.message = message;
+        dateMessage = new Date();
+    }
+
+
+
+    public String getMessage() {
+        String strDate = DateFormat.getDateInstance(DateFormat.SHORT).format(dateMessage);
+
+        return
+    }
+
 }
