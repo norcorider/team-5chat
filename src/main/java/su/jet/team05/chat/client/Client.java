83c196533cac9ff82169f5fc37cedd4dd85e4b13
klu <klu44@list.ru>
2017-10-08 12:02:13 +1000

    Some fix

--- src/main/java/su/jet/team05/chat/client/Client.java
+++ src/main/java/su/jet/team05/chat/client/Client.java
@@ -17,20 +17,66 @@ public class Client {
     public static void main(String[] args) {
         try {
             ClientWork();
-        } catch (ConnectException c) {
-            System.out.println("Нет соединения с сервером");
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
+    private static PrintWriter setClientWriter(Socket client) {
+        try {
+            return (new PrintWriter(client.getOutputStream(), true));
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    private static BufferedReader setClientReader(Socket client) {
+        try {
+            return (new BufferedReader(new InputStreamReader(client.getInputStream())));
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
     private static void ClientWork() throws IOException {
         Socket client = null;
-        while(client == null || !client.isConnected()) {
+        client = clientRecconect(client);
+
+        PrintWriter printWriter = setClientWriter(client);
+        BufferedReader reader = setClientReader(client);
+
+        Scanner scanner = new Scanner(System.in);
+        Thread readThread = new Thread(() -> {
+            while (true) {
+                try {
+                    System.out.println(reader.readLine());
+                } catch (IOException e) {
+                    break;
+                }
+            }
+        });
+
+        readThread.setDaemon(true);
+        readThread.start();
+        int state = 0;
+        do {
+            try {
+                state = Message.parseMessage(scanner.nextLine(), printWriter);
+            } catch (messageException e) {
+                state = -1;
+                System.out.println(e.getMessage());
+            }
+        } while (state != 0);
+    }
+
+    private static Socket clientRecconect(Socket client) throws IOException {
+        while (client == null || !client.isConnected()) {
             try {
                 client = new Socket(IP, PORT);
-            }
-            catch (SocketException s) {
+                System.out.println("Успешное подключение к серверу!");
+            } catch (SocketException s) {
                 System.out.println("Нет соединения с сервером");
             }
 
@@ -40,36 +86,6 @@ public class Client {
                 e.printStackTrace();
             }
         }
-
-        PrintWriter out = new PrintWriter(client.getOutputStream(), true);
-        BufferedReader reader = new BufferedReader(new InputStreamReader(client.getInputStream()));
-        Scanner in = new Scanner(System.in);
-        Thread t = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                int ReadState = 0;
-                try {
-                    while (ReadState == 0) {
-                        System.out.println(reader.readLine());
-                    }
-                } catch (SocketException s) {
-                    System.out.println("Нет соединения с сервером");
-                    System.exit(-1);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-        t.setDaemon(true);
-        t.start();
-        int state = 0;
-        do {
-            try {
-                state = Message.parseMessage(in.nextLine(), out);
-            } catch (messageException e) {
-                state = -1;
-                System.out.println(e.getMessage());
-            }
-        } while (state != 0);
+        return client;
     }
 }
